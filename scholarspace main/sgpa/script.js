5954bf61a4c4668a73eb706685c53bc9c8e7b9a1
Rahul-web01 <contact.rahul82@gmail.com>
2026-01-24 11:14:44 +0530

    Update calculator code

--- scholarspace main/sgpa/script.js
+++ scholarspace main/sgpa/script.js
@@ -1,4 +1,30 @@
+// This function sends you from the Home Page to the Course Page
 function redirectToSGPACalculator() {
-    // This redirects the user to the Course Selection page
     window.location.href = "selectcourse.html";
+}
+
+// This function calculates the math
+function calculateSGPA() {
+    let credits = document.getElementsByClassName("credit");
+    let grades = document.getElementsByClassName("grade");
+    let totalPoints = 0;
+    let totalCredits = 0;
+
+    for(let i = 0; i < credits.length; i++) {
+        let c = parseFloat(credits[i].value);
+        let g = parseFloat(grades[i].value);
+        
+        // Only calculate if the user typed a number
+        if(!isNaN(c)) {
+            totalCredits += c;
+            totalPoints += (c * g);
+        }
+    }
+
+    if(totalCredits === 0) {
+        document.getElementById("result").innerText = "Please enter credits!";
+    } else {
+        let sgpa = totalPoints / totalCredits;
+        document.getElementById("result").innerText = "Your SGPA: " + sgpa.toFixed(2);
+    }
 }
